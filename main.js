c1a930d99b7a07233dc85903b555dbbc385be21e
silentium9527 <95656729+silentium9527@users.noreply.github.com>
2024-09-28 15:21:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function registerMember() {
+    alert("注册会员功能");
+    // 这里可以实现跳转到会员注册页面的逻辑
+}
+
+function searchMember() {
+    alert("查询会员功能");
+    // 这里可以实现跳转到会员查询页面的逻辑
+}
+
+function rechargeMember() {
+    alert("会员充值功能");
+    // 这里可以实现跳转到会员充值页面的逻辑
+}
