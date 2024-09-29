eb94b173c9c3ecc8054df267ce9935301fd95130
silentium9527 <95656729+silentium9527@users.noreply.github.com>
2024-09-28 20:01:21 -0400

    Update register.js

--- register/register.js
+++ register/register.js
@@ -5,6 +5,7 @@ function save() {
         dob: document.getElementById('dob').value,
         phone: document.getElementById('phone').value,
         address: document.getElementById('address').value,
+        city: document.getElementById('city').value,
     };
 
     // 保存会员信息到 localStorage
@@ -13,10 +14,9 @@ function save() {
     localStorage.setItem('members', JSON.stringify(members));
 
     alert('Registration Successful!');
-    window.location.href = "../main/main.html"; 
+    window.location.href = "../main/main.html"; //after registration, jump to the main page
 }
 
 function cancel() {
-    window.location.href = "../main/main.html"; // jump to main page
+    window.location.href = "../main/main.html"; //if click cancel, jump to main page
 }
-
