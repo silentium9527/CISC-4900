94d6745c1868555465aff6050e46db9ff5aec75f
silentium9527 <95656729+silentium9527@users.noreply.github.com>
2024-12-08 21:58:59 -0500

    Update UUIDUtil.java

--- backend/src/main/java/com/example/demo/util/UUIDUtil.java
+++ backend/src/main/java/com/example/demo/util/UUIDUtil.java
@@ -11,7 +11,7 @@ public class UUIDUtil {
         return uid;
     }
 
-    //测试
+    //test
     public static void main(String[] args) {
         String uid = UUIDUtil.getUUID();
         System.out.println("===="+uid);
